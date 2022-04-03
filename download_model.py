613eaec9604870572201569e7966a498b6970d58
EC2 Default User <ec2-user@ip-172-31-27-72.ec2.internal>
2022-04-03 22:15:38 +0000

    add model

--- download_model.py
+++ download_model.py
@@ -3,5 +3,5 @@ from mlflow.store.artifact.models_artifact_repo import ModelsArtifactRepository
 from mlflow.tracking import MlflowClient
 
 client = MlflowClient()
-my_model = client.download_artifacts("6295d5a5af704769ac898c7e84de1d89", path="model", dst_path=".")
+my_model = client.download_artifacts("138e1521041148988d66818c8d3cc053", path="model", dst_path=".")
 print(f"Placed model in: {my_model}")
